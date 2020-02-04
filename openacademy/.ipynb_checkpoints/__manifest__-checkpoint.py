5af36baa5dae766981c0ced8801572b660b573c8
dunglan <dung.lan@gmail.com>
2020-02-04 00:24:58 +0000

    open academeny

---
+++ openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OpenAcademy",
+
+    'summary': """
+       Some training""",
+
+    'description': """
+        Some stuff
+    """,
+
+    'author': "caliva",
+    'website': "http://www.caliva.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Academy',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+       
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+      
+    ],
+}
