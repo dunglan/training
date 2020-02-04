61db0d0313b6ed72665d23932db177227376d9fb
dunglan <dung.lan@gmail.com>
2020-02-04 20:26:11 +0000

    adding tags

--- openacademy/models/.ipynb_checkpoints/course-checkpoint.py
+++ openacademy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -2,9 +2,23 @@
 
 from odoo import models, fields
 
+class OpenAcademyTags(models.Model):
+    _name = 'openacademy.tags'
+    _description = 'OpenAcademy Tags'
+    
+    name = fields.Char(string='Name')
+
 class OpenAcademyCourse(models.Model):
     _name = 'openacademy.course'
     _description = 'OpenAcademy Course'
     
     name = fields.Char(string='Course Title', required=True, indexed=True, help='Enter your course title')
+    description = fields.Html(string='Description')
+    banner = fields.Binary(string='Banner')
+    price = fields.Float(string='Price', digits=[5,4])
+    expire_date = fields.Date(string='Expire after', required=True)
+    
+    responsible_id = fields.Many2one(string='Responsible', comodel_name='res.users', required=True)
+    
+    tag_ids = fields.Many2many(comodel_name='openacademy.tags', relation='rel_course_tags', column1='course_id', column2='tag_id')
 
